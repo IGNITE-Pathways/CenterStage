2c955a5521cbdd8d0c2726d1984200294406bcd3
Ronav Gupta <75178927+guptaronav@users.noreply.github.com>
2023-10-31 16:51:00 -0700

    Using XBotOpMode
    Using XBotOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
@@ -27,15 +27,7 @@ import java.util.concurrent.TimeUnit;
  * This is the main manual OpMode
  */
 @TeleOp(name = "Manual Drive", group = "Concept")
-public class TeleOpDrive extends LinearOpMode {
-
-    //Define motors and sensors
-    DcMotor rightFront, leftFront, rightBack, leftBack = null;
-    DcMotor leftArmMotor, rightArmMotor = null;
-    Servo wristServo, leftClawServo, rightClawServo = null;
-    DistanceSensor sensorDistance = null;
-    TouchSensor leftClawTouchSensor, rightClawTouchSensor = null;
-    boolean leftPixelInClaw, rightPixelInClaw = false;
+public class TeleOpDrive extends XBotOpMode {
 
     double wristPosition = STARTING_WRIST_POSITION;
     double leftClawPosition = STARTING_CLAW_POS;
@@ -43,17 +35,11 @@ public class TeleOpDrive extends LinearOpMode {
 
     //Robot Speed
     double driveSpeed = MAX_SPEED;
-    GameMode gameMode = GameMode.NONE;
-    Boolean gameModeChanged = Boolean.FALSE;
 
     // Declare OpMode members for each of the 4 motors.
     private final ElapsedTime runtime = new ElapsedTime();
     Queue<Double> distanceQueue = new SizeLimitedQueue<>(10);
     double calculatedDistance = DistanceSensor.distanceOutOfRange;
-
-    private VisionPortal visionPortal;               // Used to manage the video source.
-    private AprilTagProcessor aprilTag;              // Used for managing the AprilTag detection process.
-    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
 
     @Override
     public void runOpMode() {
@@ -66,6 +52,8 @@ public class TeleOpDrive extends LinearOpMode {
 
         // Initialize all motors and sensors
         initialize();
+        resetWristAndClawPosition();
+
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -214,6 +202,11 @@ public class TeleOpDrive extends LinearOpMode {
         }
     }
 
+    private void resetWristAndClawPosition() {
+        wristServo.setPosition(STARTING_WRIST_POSITION);
+        leftClawServo.setPosition(STARTING_CLAW_POS);
+        rightClawServo.setPosition(STARTING_CLAW_POS);
+    }
     private void waitAndMoveArmAndResetDistance() {
         sleep(1000);
         // Move Arm up to remove friction and get clearance the ground
@@ -252,24 +245,6 @@ public class TeleOpDrive extends LinearOpMode {
         return calculatedDistance < 40;
     }
 
-    private boolean detectAprilTags() {
-        boolean aprilTagFound = false;
-        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
-        for (AprilTagDetection detection : currentDetections) {
-            if ((detection.metadata != null) &&
-                    ((XBot.DESIRED_TAG_ID < 0) || (detection.id == XBot.DESIRED_TAG_ID))) {
-                aprilTagFound = true;
-                //We found April Tag, change game mode to APRIL TAG NAV
-                changeGameMode(GameMode.APRIL_TAG_NAVIGATION);
-                desiredTag = detection;
-                break;  // don't look any further.
-            } else {
-                telemetry.addData("Unknown Target", "Tag ID %d is not in TagLibrary\n", detection.id);
-            }
-        }
-        return aprilTagFound;
-    }
-
     private void dropPixels() {
         leftClawServo.setPosition(CLAW_OPEN_POSITION);
         rightClawServo.setPosition(CLAW_OPEN_POSITION);
@@ -277,71 +252,6 @@ public class TeleOpDrive extends LinearOpMode {
         leftPixelInClaw = false;
         rightPixelInClaw = false;
         changeGameMode(GameMode.GOING_TO_PICK_PIXELS);
-    }
-
-    private void changeGameMode(GameMode mode) {
-        gameMode = mode;
-        gameModeChanged = Boolean.TRUE;
-        gamepad2.rumble(1000);
-    }
-
-    private void initialize() {
-        gameMode = GameMode.INIT;
-        // Initialize the hardware variables. Note that the strings used here must correspond
-        // to the names assigned during the robot configuration step on the DS or RC devices.
-        leftFront = hardwareMap.get(DcMotor.class, "leftfront");
-        leftBack = hardwareMap.get(DcMotor.class, "leftback");
-        rightFront = hardwareMap.get(DcMotor.class, "rightfront");
-        rightBack = hardwareMap.get(DcMotor.class, "rightback");
-
-        // Initialize Motors
-        leftArmMotor = hardwareMap.get(DcMotor.class, "leftArmMotor");
-        rightArmMotor = hardwareMap.get(DcMotor.class, "rightArmMotor");
-
-        // Initialize Rev 2M Distance sensor
-        sensorDistance = hardwareMap.get(DistanceSensor.class, "distance");
-        leftClawTouchSensor = hardwareMap.get(TouchSensor.class, "leftClawTouch");
-        rightClawTouchSensor = hardwareMap.get(TouchSensor.class, "rightClawTouch");
-
-        // Initialize April Tag
-        aprilTag = new AprilTagProcessor.Builder().build();
-        visionPortal = new VisionPortal.Builder()
-                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-                .addProcessor(aprilTag)
-                .build();
-        setManualExposure(6, 250);  // Use low exposure time to reduce motion blur
-
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
-        leftBack.setDirection(DcMotor.Direction.REVERSE);
-        rightFront.setDirection(DcMotor.Direction.FORWARD);
-        rightBack.setDirection(DcMotor.Direction.FORWARD);
-
-        //Left Motor is in reverse
-        leftArmMotor.setDirection(DcMotor.Direction.REVERSE);
-
-        //Using Encoders
-        leftArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        //Initialize Servo
-        wristServo = hardwareMap.get(Servo.class, "wrist");
-        leftClawServo = hardwareMap.get(Servo.class, "leftClaw");
-        rightClawServo = hardwareMap.get(Servo.class, "rightClaw");
-
-        //Reset encoders -- making start position as zero
-        resetArmEncoders();
-        resetWristAndClawPosition();
-    }
-
-    private void resetArmEncoders() {
-        leftArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    }
-
-    private void resetWristAndClawPosition() {
-        wristServo.setPosition(STARTING_WRIST_POSITION);
-        leftClawServo.setPosition(STARTING_CLAW_POS);
-        rightClawServo.setPosition(STARTING_CLAW_POS);
     }
 
     private int goToPickPixelPosition() {
@@ -420,12 +330,12 @@ public class TeleOpDrive extends LinearOpMode {
 
     public void moveRobotBack() {
         leftFront.setPower(-0.2);
-        rightBack.setPower(-0.2);
+        rightFront.setPower(-0.2);
         leftBack.setPower(-0.2);
         rightBack.setPower(-0.2);
         sleep(1000);
         leftFront.setPower(0);
-        rightBack.setPower(0);
+        rightFront.setPower(0);
         leftBack.setPower(0);
         rightBack.setPower(0);
     }
@@ -454,39 +364,6 @@ public class TeleOpDrive extends LinearOpMode {
         rightBack.setPower(rightFrontPower);
         leftBack.setPower(leftBackPower);
         rightBack.setPower(rightBackPower);
-    }
-
-    private void setManualExposure(int exposureMS, int gain) {
-        // Wait for the camera to be open, then use the controls
-
-        if (visionPortal == null) {
-            return;
-        }
-
-        // Make sure camera is streaming before we try to set the exposure controls
-        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
-            telemetry.addData("Camera", "Waiting");
-            telemetry.update();
-            while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
-                sleep(20);
-            }
-            telemetry.addData("Camera", "Ready");
-            telemetry.update();
-        }
-
-        // Set camera controls unless we are stopping.
-        if (!isStopRequested()) {
-            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
-            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
-                exposureControl.setMode(ExposureControl.Mode.Manual);
-                sleep(50);
-            }
-            exposureControl.setExposure(exposureMS, TimeUnit.MILLISECONDS);
-            sleep(20);
-            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
-            gainControl.setGain(gain);
-            sleep(20);
-        }
     }
 
     private void addNewDistanceValue(double distance) {
