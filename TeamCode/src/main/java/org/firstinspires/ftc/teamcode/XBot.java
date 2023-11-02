3d181de989ce7f6ee605646ef3d1e58aa24e5b6e
Ronav Gupta <75178927+guptaronav@users.noreply.github.com>
2023-11-01 19:56:55 -0700

    Changed Robot Max Speed for now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XBot.java
@@ -5,7 +5,7 @@ public class XBot {
     static final int MAX_ARM_POSITION = 500;
     static final int MIN_ARM_POSITION = 0;
     static final int FULL_CIRCLE = 1075;
-    static final int ARM_PICK_POSITION = MIN_ARM_POSITION + 5; //Robot must move slow
+    static final int ARM_PICK_POSITION = MIN_ARM_POSITION + 3; //Robot must move slow
     static final int ARM_POSITION_HIGH = MIN_ARM_POSITION + 20; //Robot running across field
     static final double ARM_SPEED = 0.3;
 
@@ -21,7 +21,7 @@ public class XBot {
     static final double STARTING_CLAW_POS = CLAW_OPEN_POSITION;
 
     //DRIVE
-    static final double MAX_SPEED = 1.0;
+    static final double MAX_SPEED = 0.7; //Increase as operator gets better
     static final double SPEED_WHEN_PICKING_PIXELS = 0.3; //gameMode = PICKING_PIXELS
     static final double SPEED_WHEN_DROPPING_PIXELS = 0.3; //gameMode = DROPPING_PIXELS
     static final double SPEED_WHEN_ON_APRIL_TAG_NAV = 0.5; //gameMode = APRIL_TAG_NAVIGATION
