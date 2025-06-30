60e666ad1408bd26312d19cf07af4bf33f912835
Arjun <Arjun's computer>
2025-06-30 14:47:26 -0700

    updated gradle files and meep meep problems

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting_Far.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting_Far.java
@@ -2,12 +2,12 @@ package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
-import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
-import com.noahbres.meepmeep.roadrunner.DriveTrainType;
-import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
+import org.rowlandhall.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
+import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
+import org.rowlandhall.meepmeep.roadrunner.DriveTrainType;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting_Far {
     public static void main(String[] args) {
